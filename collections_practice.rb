60e844ab544ac36fc930d051e1131a96c1db966a
Matt Fender <mjfender@gmail.com>
2016-12-12 14:29:54 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def begins_with_r(array)
   if array.any? do |tool|
     tool[0,1] != "r"
@@ -26,33 +28,93 @@ end
 
 def remove_non_strings(array)
   array.delete_if do |item|
-   puts item.class
    item.is_a?(Fixnum) || item.is_a?(Symbol)
   end
 end
 
 def count_elements(array)
 # array is: [{:name=>"blake"}, {:name=>"blake"}, {:name=>"ashley"}]
-counts = Hash.new 0
+# helpful hash methods... .has_value?(asdf), .values
 
-  array.each do |element|
-  # Iterate through provided array to get all hashes
-    puts "#{element[:name]} is element[:name]"
-    counts[:name] = element[:name]
-    puts "#{counts[:name]} is counts[:name]"
-    puts "#{counts[:name][:count]} is counts[:name][:count]"
-    #counts[:name][:count] += 1
-    puts "#{counts}"
+  just_names = []
+  new_array = []
+  new_hash = {}
+
+  just_names = array.collect do |hash| 
+    hash.values
+  end.flatten.sort.reverse
+
+  just_names.each do |name|
+    if new_array.count > 0
+      if new_array[-1][:name] == name
+        new_array[-1][:count] +=1
+      else
+        new_hash[:name] = name
+        new_hash[:count] = 1
+        new_array << new_hash
+        new_hash = {}
+      end
+    else
+      new_hash[:name] = name
+      new_hash[:count] = 1
+      new_array << new_hash
+      new_hash = {}
+    end
+
   end
-
+  new_array
 end
 
 def merge_data(array1, array2)
-  array1.concat(array2)
+  # array1 --> [{:first_name=>"blake"}, {:first_name=>"ashley"}]
+  # array2 --> [{"blake"=>
+  #  {:awesomeness=>10,
+  #   :height=>"74",
+  #   :last_name=>"johnson"},
+  # "ashley"=>
+  #  {:awesomeness=>9, :height=>60, :last_name=>"dubs"}}]
+  # expect:
+  # [{:first_name=>"blake", :awesomeness=>10, :height=>"74", :last_name=>"johnson"}, {:first_name=>"ashley", :awesomeness=>9, :height=>60, :last_name=>"dubs"}]
+
+  new_hashes = []
+  array1.each do |hash|
+    new_hash = {}
+    new_hash[hash.keys.first]= hash[hash.keys.first]
+    array2[0].each do |key, value|
+      if new_hash[hash.keys.first] = key
+        value.each do |k, v|
+        new_hash[k] = v
+        end
+      end
+    new_hashes << new_hash
+    new_hash = {}
+    end
+  end
+  new_hashes.uniq
 end
 
 def find_cool(array)
+  array.select do |hash|
+    hash[:temperature] == "cool"
+  end
 end
 
 def organize_schools(array)
+ #  array is {"flatiron school bk"=>{:location=>"NYC"},
+ # "flatiron school"=>{:location=>"NYC"},
+ # "dev boot camp"=>{:location=>"SF"},
+ # "dev boot camp chicago"=>{:location=>"Chicago"},
+ # "general assembly"=>{:location=>"NYC"},
+ # "Hack Reactor"=>{:location=>"SF"}}
+  
+  #expects {"NYC"=>["flatiron school bk", "flatiron school", "general assembly"], "SF"=>["dev boot camp", "Hack Reactor"], "Chicago"=>["dev boot camp chicago"]}
+  new_hash = {}
+  array.each do |array2|
+  #school is ["flatiron school bk", {:location=>"NYC"}]
+        
+        new_hash[array2[1].values[0]] ||= []
+        new_hash[array2[1].values[0]] << array2[0]
+  end
+  new_hash
+  
 end
